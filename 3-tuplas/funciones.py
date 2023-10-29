ea1e33f7aa2816252b5d8fe8783cd7c2989bcb36
javier <javier.morabaiz@gmail.com>
2023-10-29 16:34:52 -0300

    modulo tuplas

---
+++ 3-tuplas/funciones.py
@@ -0,0 +1,10 @@
+numeros = (1, 2, 3, 4, 5, 6, 9, 8, 7)
+print(len(numeros))
+
+print(max(numeros))
+print(min(numeros))
+
+print(sum(numeros))
+print(sorted(numeros))
+print(type(numeros))
+print(sorted(numeros, reverse=True))
